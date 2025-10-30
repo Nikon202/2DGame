a34a3095fc02c01c52cf0468db77544f2e75c24d
Nikon202 <cuprovnikon@gmail.com>
2025-10-30 16:15:22 +0300

    DamageEnemy

--- Assets/Scripts/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -5,25 +5,37 @@ using System;
 
 public class EnemyAttack : MonoBehaviour
 {
+    CharHp charHp;
+    bool isAttack;
+    
     private Transform trPerson;
     Transform trEnemy;
  public float minDistanceAttack = 3;
- public event Action<bool> onAttackAnim;
+    public event Action<bool> onAttackAnim;
+    float timer;
+    public float interval = 0.5f;
+    public float damage = 5;
  private void Start()
  {
         trEnemy = transform;
         trPerson = FindObjectOfType<CharacterMove>().transform;
+        charHp = trPerson.GetComponent<CharHp>();
 
  }
 
  void Update()
  {
      float dist = Vector2.Distance(trPerson.position, trEnemy.position);
-     IsMinDistance(dist);
+        IsMinDistance(dist);
+        if (isAttack && timer < Time.time)
+        {
+            timer = Time.time + interval;
+            charHp.TakeDamage(damage);
+        }
  }
  public void IsMinDistance(float dist)
  {
-     bool isAttack = minDistanceAttack >= dist;
-     onAttackAnim?.Invoke(isAttack); 
+     isAttack = minDistanceAttack >= dist;
+        onAttackAnim?.Invoke(isAttack); 
  }
 }
